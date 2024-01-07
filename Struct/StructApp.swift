b3e4e3f878d9ac174895e00abbdf5435f15c1fc7
enochchigbu <enochchigbu@gmail.com>
2024-01-06 23:09:11 -0800

    Initial Commit

---
+++ Struct/StructApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructApp.swift
+//  Struct
+//
+//  Created by Enoch Chigbu on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct StructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
